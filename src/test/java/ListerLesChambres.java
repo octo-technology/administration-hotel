13744e7eddea5b3fb8e007a7af51515063e95d7f
BGchristophe <c.breheret-girardin@octo.com>
2023-05-08 14:33:04 +0200

    Init structure

---
+++ src/test/java/ListerLesChambres.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class ListerLesChambres {
+
+	@Test
+	void testStructure() {
+		assertEquals(1, 1);
+	}
+	
+}
